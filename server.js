bc5eaaf945e867b7552249aa79718741cfb4fc50
Abdullah Adel <badl00707@gmail.com>
2026-08-08 09:24:15 +0300

    Enable clean URLs and remove .html extensions

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 
 // Basic health check endpoint
 app.get('/api/health', (req, res) => {
